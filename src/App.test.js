e2c8e92341dd2d54dad61515770cb4c5c612402f
mikemo <mike.mo@thedailyedited.com>
2018-08-10 19:20:38 +1000

    Link #7 todo list, display tods and pass the acceptance test(e2e test)

--- src/App.test.js
+++ src/App.test.js
@@ -5,21 +5,10 @@ import { shallow } from 'enzyme';
 import { App } from './App';
 import { initialState } from './reducers/';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	ReactDOM.unmountComponentAtNode(div);
-});
-
-it('App renders without crashing version 2, test with the enzyme', () => {
-	const component = shallow(<App />);
-	expect(component.exists()).toEqual(true);
-});
-
 it('App renders without crashing version 3, the redux way', () => {
 	const mockFunction = jest.fn();
 
-	const component = shallow(<App state={initialState} submitTodo={mockFunction} />);
+	const component = shallow(<App state={initialState} submitTodo={mockFunction} todos={[]} />);
 
 	expect(component.exists()).toEqual(true);
 });

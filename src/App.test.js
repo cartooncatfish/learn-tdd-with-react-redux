28799b4df702d144d9a2831ae88c92e57e8ea6fd
mikemo <mike.mo@thedailyedited.com>
2018-08-10 10:28:10 +1000

    Link #1, first test with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
+
+it('App renders without crashing version 2, test with the enzyme', () => {
+	const component = shallow(<App />);
+	expect(component.exists()).toEqual(true);
 });

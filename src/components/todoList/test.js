32b51cce268518c1f2a3d3bc5740bd1f79364868
mikemo <mike.mo@thedailyedited.com>
2018-08-10 15:52:04 +1000

    Link #7 todo list, check the display of todo item

--- src/components/todoList/test.js
+++ src/components/todoList/test.js
@@ -3,9 +3,23 @@ import { shallow } from 'enzyme';
 import TodoList from '.';
 
 describe('TodoList component', () => {
-	const component = shallow(<TodoList />);
+	/*
+    1. define the state object which needs to be pass to the compoent
+    2. check the render result of the component of the test state
+    */
+	const todos = [
+		{
+			id: 1,
+			text: 'A todo',
+		},
+	];
+	const component = shallow(<TodoList todos={todos} />);
 
 	it('Should render successfully', () => {
 		expect(component.exists()).toEqual(true);
+	});
+
+	it('Should display a todo when passed in as a prop', () => {
+		expect(component.find('.todo-text').text()).toEqual(todos[0].text);
 	});
 });

e58006bc8e056287ea7787f41c1f2d420fc7f6a2
mikemo <mike.mo@thedailyedited.com>
2018-08-10 15:46:15 +1000

    Link #7 todo list, basic component(include test & component)

---
+++ src/components/todoList/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TodoList from '.';
+
+describe('TodoList component', () => {
+	const component = shallow(<TodoList />);
+
+	it('Should render successfully', () => {
+		expect(component.exists()).toEqual(true);
+	});
+});
